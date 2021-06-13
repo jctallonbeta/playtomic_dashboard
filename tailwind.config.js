337da4e16576a4296912009c1dd958afa2ac08dc
trueStoryJapan <juanca_mt@hotmail.com>
2021-06-13 23:31:35 +0900

    feat: set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: ["./src/**/*.{js, jsx, ts, tsx}"],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      sans: ['"Work Sans"', "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          darkest: "#004541",
+          dark: "#017069",
+          DEFAULT: "#5BBB6D",
+          light: "#a4d96c",
+          lightest: "#D4FCA9",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
