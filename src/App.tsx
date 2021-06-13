e75ab0f430bb5ad64ab510b3663d94ea4a975aff
trueStoryJapan <juanca_mt@hotmail.com>
2021-06-13 23:25:55 +0900

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Settings } from "./pages/Settings";
+import { SignIn } from "./pages/SignIn";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/settings" component={Settings} />
+          <Route exact path="/signin" component={SignIn} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
